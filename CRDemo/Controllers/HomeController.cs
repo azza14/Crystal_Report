ff39ddae0c6b1281336ff5a820d4897ea2a44b68
azza <azzaashry14@gmail.com>
2021-08-31 15:51:02 +0200

    Add project files.

---
+++ CRDemo/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CRDemo.Models;
+using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRDemo.Controllers
+{
+    public class HomeController : Controller
+    { 
+        NewWebsitDBEntities context= new NewWebsitDBEntities();
+        public ActionResult Index()
+        {
+            return View( context.News.ToList());
+        }
+        public ActionResult ExportReport()
+        {
+            var rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Report"), "CrystalReport.rpt"));
+            rd.SetDataSource(context.News.ToList());
+           
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+            rd.PrintOptions.PaperOrientation = CrystalDecisions.Shared.PaperOrientation.Landscape;
+            rd.PrintOptions.ApplyPageMargins(new CrystalDecisions.Shared.PageMargins(5, 5, 5, 5));
+            rd.PrintOptions.PaperSize = CrystalDecisions.Shared.PaperSize.PaperA5;
+
+
+            try
+            {
+                var stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                stream.Seek(0, SeekOrigin.Begin);
+                return File(stream, "application/pdf", "news_Report.pdf"); 
+            }
+            catch
+            {
+                throw;
+            }
+
+
+
+
+        }
+
+
+    }
+}
